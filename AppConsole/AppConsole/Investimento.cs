d8d8c714a23c5a2dc3934f89ae9e6b569dc2853b
Camila Santos <camilasjc16@hotmail.com>
2021-03-28 19:14:11 -0300

    First option

--- AppConsole/AppConsole/Investimento.cs
+++ AppConsole/AppConsole/Investimento.cs
@@ -1,45 +1,27 @@
 ﻿namespace AppConsole {
     class Investimento {
 
-        private string _nome; // Variavel do nome do ativo
-        private double _vTotal() { // Variavel do valor total do ativo
-            return Qte * VUnit;
-        }
-        private double _somatorio; // Variavel do valor total investido
 
+        
+        private double _somatorio; // Variavel do valor total investido
+        public string Nome; // Variavel do nome do ativo
         public int Id { get; set; } // Identificação do ativo na lista
         public int Qte { get; set; } // Variavel da quantidade do ativo adquirida
         public double VUnit { get; set; } // Variavel do valor unitário de cada ação
         public string DtHr { get; set; } // Variavel da data/hora do registro
+        public double VTotal { get; set; } // Variavel do valor total do ativo
 
+       
         public Investimento() {
+           
         }
-        public Investimento(string nome, double vtotal) {
-            _nome = nome;
-        }
-
-        /*public void Setvtotal(double vtotal) {
-            bool v = vtotal != null;
-            if (v) {
-                _vTotal = vtotal;
-            }
-
-        }*/
-
-        public string GetNome() {
-            return _nome;
-        }
-        public void SetNome(string nome) { // Condição para que o nome do ativo correto
-            if (nome != null && nome.Length > 1 && nome.Length < 4) {
-                _nome = nome;
-            }
-
+        public Investimento(string Nome, double vtotal) {
         }
 
-        public void AdicionarAtivos(int quantidade) {// Método adicionar ativos
 
+        public void AdicionarAtivos(string Nome, double qte, double VUnit, double _vTotal, string DtHr) {// Método adicionar ativos
         }
-        public void RemoverAtivos(int quantidade) {// Método remover ativos
+        public void RemoverAtivos(string Nome, double qte, double VUnit, double _vTotal) {// Método remover ativos
 
         }
     }
--- AppConsole/AppConsole/Program.cs
+++ AppConsole/AppConsole/Program.cs
@@ -1,21 +1,69 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
 
-namespace AppConsole
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Bem - vindo(a) a sua Carteira de Investimentos!\n" +
-                    "Aplicativo feito para você Investidor!\n" +
-                    "Nesse APP voce pode ter uma visao global dos seus investimentos\n" +
-                    "Utilize as nossas interações e acompanhe sua carteira:\n\n"); //Mensagem de inicialização
+namespace AppConsole {
+    class Program {
+        static void Main(string[] args) {
+            int opcao = 0; //Variável para escolha do menu
+            while (opcao != 5) { //Fórmula quando o usuário digitar algo  errado
 
-            Console.Write("Digite 1 - Para registrar a compra de um novo ativo na carteira\n" +
-                "Digite 2 - Para visualizar a lista de ativos investidos\n" +
-                "Digite 3 - Para informar a venda de seus ativos\n" +
-                "Digite 4 - Visualizar um resumo do valor total investido em todos os ativos\n" +
-                "Digite 5 - Para sair de sua Carteira de Investimentos\n"); //Descrição das operações a serem escolhidas pelo usuário
+
+                Console.WriteLine("Bem - vindo(a) a sua Carteira de Investimentos!\n" +
+                        "Aplicativo feito para você Investidor!\n" +
+                        "Nesse APP voce pode ter uma visao global dos seus investimentos\n" +
+                        "Utilize as nossas interações e acompanhe sua carteira:\n\n"); //Mensagem de inicialização
+
+                Console.Write("Digite 1 - Para registrar a compra de um novo ativo na carteira\n" +
+                    "Digite 2 - Para visualizar a lista de ativos investidos\n" +
+                    "Digite 3 - Para informar a venda de seus ativos\n" +
+                    "Digite 4 - Visualizar um resumo do valor total investido em todos os ativos\n" +
+                    "Digite 5 - Para sair de sua Carteira de Investimentos\n"); //Descrição das operações a serem escolhidas pelo usuário
+
+                Console.WriteLine(" ");
+                opcao = int.Parse(Console.ReadLine()); //Fórmula para ler a opção do usuário
+
+                Console.WriteLine("Você digitou a opção:" + opcao); //Confirmação da opção desejada
+                Console.Clear();
+
+                List<Investimento> list = new List<Investimento>(); //Criação da lista a ser adicionada.
+
+                int s = 0;
+
+                if (opcao == 1) ;{
+                    if (s != 9) {
+                        Console.WriteLine("Para continuar aperte enter, se quiser voltar aperte 9");
+                        s = int.Parse(Console.ReadLine());
+                        Investimento p = new Investimento();
+                        Console.Clear();
+                        Console.WriteLine("Você digitou a opção:" + opcao);
+                        Console.WriteLine("Digite o nome do ativo");
+                        p.Nome = Console.ReadLine();
+                        Console.WriteLine("Quantos deseja comprar?");
+                        p.Qte = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Qual valor unitário?");
+                        p.VUnit = double.Parse(Console.ReadLine());
+                        p.DtHr = Convert.ToString(DateTime.Now);
+
+                        Console.WriteLine("Confirme as informações:");
+                        Console.WriteLine("Nome:"
+                                          + p.Nome
+                                          + "Quantidade:"
+                                          + p.Qte
+                                          + "Valor Unitário:"
+                                          + p.VUnit
+                                          + "Valor Total ="
+                                          + p.Qte * p.VUnit);
+                    }
+                    else ;
+                    {
+                    };
+                
+                
+                };
+            
+            }
+            
         }
     }
 }
