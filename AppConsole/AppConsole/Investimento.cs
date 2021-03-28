f78ccdf3f6fa316715600e4162f28464443ec2e9
Camila Santos <camilasjc16@hotmail.com>
2021-03-28 17:42:33 -0300

    Initial Commit

---
+++ AppConsole/AppConsole/Investimento.cs
@@ -0,0 +1,46 @@
+﻿namespace AppConsole {
+    class Investimento {
+
+        private string _nome; // Variavel do nome do ativo
+        private double _vTotal() { // Variavel do valor total do ativo
+            return Qte * VUnit;
+        }
+        private double _somatorio; // Variavel do valor total investido
+
+        public int Id { get; set; } // Identificação do ativo na lista
+        public int Qte { get; set; } // Variavel da quantidade do ativo adquirida
+        public double VUnit { get; set; } // Variavel do valor unitário de cada ação
+        public string DtHr { get; set; } // Variavel da data/hora do registro
+
+        public Investimento() {
+        }
+        public Investimento(string nome, double vtotal) {
+            _nome = nome;
+        }
+
+        /*public void Setvtotal(double vtotal) {
+            bool v = vtotal != null;
+            if (v) {
+                _vTotal = vtotal;
+            }
+
+        }*/
+
+        public string GetNome() {
+            return _nome;
+        }
+        public void SetNome(string nome) { // Condição para que o nome do ativo correto
+            if (nome != null && nome.Length > 1 && nome.Length < 4) {
+                _nome = nome;
+            }
+
+        }
+
+        public void AdicionarAtivos(int quantidade) {// Método adicionar ativos
+
+        }
+        public void RemoverAtivos(int quantidade) {// Método remover ativos
+
+        }
+    }
+}
---
+++ AppConsole/AppConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem - vindo(a) a sua Carteira de Investimentos!\n" +
+                    "Aplicativo feito para você Investidor!\n" +
+                    "Nesse APP voce pode ter uma visao global dos seus investimentos\n" +
+                    "Utilize as nossas interações e acompanhe sua carteira:\n\n"); //Mensagem de inicialização
+
+            Console.Write("Digite 1 - Para registrar a compra de um novo ativo na carteira\n" +
+                "Digite 2 - Para visualizar a lista de ativos investidos\n" +
+                "Digite 3 - Para informar a venda de seus ativos\n" +
+                "Digite 4 - Visualizar um resumo do valor total investido em todos os ativos\n" +
+                "Digite 5 - Para sair de sua Carteira de Investimentos\n"); //Descrição das operações a serem escolhidas pelo usuário
+        }
+    }
+}
