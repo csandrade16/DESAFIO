34a680eaabfade1c44853432ce5d8de2b5e3c4aa
Camila Santos <camilasjc16@hotmail.com>
2021-03-29 12:26:18 -0300

    Third option

--- AppConsole/AppConsole/Program.cs
+++ AppConsole/AppConsole/Program.cs
@@ -29,44 +29,86 @@ namespace AppConsole {
                 List<Investimento> list = new List<Investimento>(); //Criação da lista a ser adicionada.
 
                 int s = 0;
+                int v = 0;
+                int t = 0;
 
-                if (opcao == 1) 
-                {
-                    Investimento p = new Investimento();
+                if (opcao == 1) {
+                    Console.WriteLine("Você digitou a opção:" + opcao
+                            + "\nSe deseja cancelar, aperte 9;" + //Opção de retornar ao menu
+                            "\nPara prosseguir aperte 1 novamente"); 
+                    s = int.Parse(Console.ReadLine());
+
+                    if (s == 1) {
+
+                        Investimento p = new Investimento();
+                        Console.Clear();
+                        Console.WriteLine("Digite o nome do ativo");
+                        p.Nome = Console.ReadLine();
+                        Console.WriteLine("Quantos deseja comprar?");
+                        p.Qte = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Qual valor unitário?");
+                        p.VUnit = double.Parse(Console.ReadLine());
+                        p.DtHr = Convert.ToString(DateTime.Now);
+
+                        Console.WriteLine("Confirme as informações:");
+                        Console.WriteLine("Nome:"
+                                          + p.Nome
+                                          + "Quantidade:"
+                                          + p.Qte
+                                          + "Valor Unitário:"
+                                          + p.VUnit
+                                          + "Valor Total ="
+                                          + p.Qte * p.VUnit);
+                        list.Add(p);
+                    }
                     Console.Clear();
-                    Console.WriteLine("Você digitou a opção:" + opcao);
-                    Console.WriteLine("Digite o nome do ativo");
-                    p.Nome = Console.ReadLine();
-                    Console.WriteLine("Quantos deseja comprar?");
-                    p.Qte = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Qual valor unitário?");
-                    p.VUnit = double.Parse(Console.ReadLine());
-                    p.DtHr = Convert.ToString(DateTime.Now);
-
-                    Console.WriteLine("Confirme as informações:");
-                    Console.WriteLine("Nome:"
-                                      + p.Nome
-                                      + "Quantidade:"
-                                      + p.Qte
-                                      + "Valor Unitário:"
-                                      + p.VUnit
-                                      + "Valor Total ="
-                                      + p.Qte * p.VUnit);
                 }
 
-                
-                else if (opcao == 2) {
-                    Console.WriteLine("Veja seu extrato a seguir:");
 
-                    foreach (Investimento p in list ) {
-                        Console.WriteLine("Nome:" + p.Nome + "; Quantidade:" + p.Qte + "; Valor Unitário: R$" + p.VUnit + ";Total: R$" + p.VTotal + "|" + p.DtHr);
+                else if (opcao == 2) {
+                    Console.WriteLine("Você digitou a opção:" + opcao
+                            + "\nSe deseja cancelar, aperte 9;" + //Opção de retornar ao menu
+                            "\nPara prosseguir aperte 2 novamente"); 
+                    v = int.Parse(Console.ReadLine());
+
+                    while (s == 2) {
+                        Console.WriteLine("Veja seu extrato a seguir:");
+
+                        foreach (Investimento p in list) {
+                            Console.WriteLine("Nome:" + p.Nome + "; Quantidade:" + p.Qte + "; Valor Unitário: R$" + p.VUnit + ";Total: R$" + p.VTotal + "|" + p.DtHr);
+                        }
+
+                        Console.WriteLine("Para voltar ao menu anterior tecle Enter");
+                        Console.ReadLine();
+                    }
+                    Console.Clear();
+                }
+
+                else if (opcao == 3) {
+                    Console.WriteLine("Você digitou a opção:" + opcao
+                           + "\nSe deseja cancelar, aperte 9;" + //Opção de retornar ao menu
+                           "\nPara prosseguir aperte 3 novamente"); 
+                    t = int.Parse(Console.ReadLine());
+
+                    while (t == 3) {
+
+                        Console.WriteLine("Qual ativo deseja remover?");
+                        int indice = 1;
+                        // Para cada ativo na minha lista
+
+                        foreach (Investimento p in list) {
+                            Console.WriteLine("ID:" + indice + "Nome:" + p.Nome 
+                                + "; Quantidade:" + p.Qte 
+                                + "; Valor Unitário: R$" + p.VUnit 
+                                + ";Total: R$" + p.VTotal);
+                            indice++;
+
+                        }
+                        list.RemoveAt(int.Parse(Console.ReadLine()));
                     }
 
-                    Console.WriteLine("Para voltar ao menu anterior tecle Enter");
-                    Console.ReadLine();
+
                 }
-
-
             }
 
         }
